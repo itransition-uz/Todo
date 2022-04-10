5a85c2fcde4950c461428e876eb2de21d51f742c
SaburovBabur <saburovbabur@gmail.com>
2022-04-10 15:13:45 +0500

    🔧 Test Branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 function App(props) {
   return (
     <>
       <Button variant="contained">Hello World</Button>
+      <Checkbox {...label} defaultChecked />
       <h1>Itransition Webpack!</h1>
     </>
   );
