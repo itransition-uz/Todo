9e35e36852efda8b92b727578a1ce3ff0d17d411
SaburovBabur <saburovbabur@gmail.com>
2022-04-13 03:04:13 +0500

    👌 Do some `tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 
 function App() {
+  const [count, setCount] = useState<number>(0);
+
   return (
     <>
-      <Button variant="contained">Hello World</Button>
-      <h1>Itransition Webpack!</h1>
+      <h1>{count}</h1>
+
+      <Button onClick={increment} variant="outlined" size="medium">
+        +
+      </Button>
+
+      <Button onClick={decrement} variant="outlined" size="medium">
+        -
+      </Button>
     </>
   );
+
+  function increment(): void {
+    setCount((count) => count + 1);
+  }
+
+  function decrement(): void {
+    if (count > 0) {
+      setCount((count) => count - 1);
+    }
+  }
 }
 
 export default App;
