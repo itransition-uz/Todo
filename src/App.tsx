00b92b4e62f4110771c9fc700e292167ad40d2fd
SaburovBabur <saburovbabur@gmail.com>
2022-04-13 14:27:16 +0500

    ✅ MaterialUI Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,136 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Badge from "@mui/material/Badge";
+import { teal } from "@mui/material/colors";
+import { Container, Grid } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Stack from "@mui/material/Stack";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import Typography from "@mui/material/Typography";
+import Checkbox from "@mui/material/Checkbox";
+
+interface Todo {
+  id: string;
+  title: string;
+  completed_at?: Date;
+  deleted_at?: Date;
+}
 
 function App() {
-  const [count, setCount] = useState<number>(0);
+  const [data, setData] = useState<Todo[]>([
+    {
+      id: "1234",
+      title: "Typescript + MaterialUI",
+    },
+    {
+      id: "1234",
+      title: "Typescript + MaterialUI",
+      completed_at: new Date(),
+    },
+  ]);
 
   return (
     <>
-      <h1>{count}</h1>
+      <Box
+        sx={{
+          width: `100%`,
+          height: 75,
+          backgroundColor: teal.A400,
+          display: `flex`,
+          justifyContent: `center`,
+          alignItems: `center`,
+          color: `white`,
+        }}
+      >
+        <h2>
+          <Badge badgeContent={4} color={"info"}>
+            Ежедневные задачи:
+          </Badge>
+        </h2>
+      </Box>
 
-      <Button onClick={increment} variant="outlined" size="medium">
-        +
-      </Button>
+      <Box
+        sx={{
+          width: `100%`,
+          display: `flex`,
+          justifyContent: `center`,
+          alignItems: `center`,
+          flexDirection: `column`,
+          padding: `2rem 0rem`,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="standard-basic"
+                label="Задача"
+                variant="standard"
+                onKeyPress={(e) => {
+                  e.key === `Enter` ? console.log(e) : "";
+                }}
+              />
+            </Grid>
+            {data.map((todo) => (
+              <Grid
+                container
+                sx={{
+                  paddingTop: `1rem`,
+                }}
+                alignItems="center"
+              >
+                <Grid xs={2}>
+                  <Checkbox checked={!!todo.completed_at} />
+                </Grid>
 
-      <Button onClick={decrement} variant="outlined" size="medium">
-        -
-      </Button>
+                <Grid xs={8}>
+                  <Typography
+                    sx={{
+                      textDecoration: todo.completed_at ? `line-through` : "",
+                    }}
+                  >
+                    MaterialUI + Typescript
+                  </Typography>
+                </Grid>
+
+                <Grid xs={2}>
+                  <Stack direction="row" alignItems="center" spacing={1}>
+                    <IconButton aria-label="delete" size="small">
+                      <ModeEditIcon fontSize="small" />
+                    </IconButton>
+
+                    <IconButton aria-label="delete" size="small">
+                      <DeleteIcon fontSize="small" />
+                    </IconButton>
+                  </Stack>
+                </Grid>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
     </>
   );
 
   function increment(): void {
-    setCount((count) => count + 1);
-  }
+    setData((data) =>
+      data.concat({
+        id: "123",
+        title: "Typescript + MaterialUI",
+      })
+    );
 
-  function decrement(): void {
-    if (count > 0) {
-      setCount((count) => count - 1);
-    }
+    setData((data) =>
+      data.concat({
+        id: "1234",
+        title: "Typescript + MaterialUI",
+        completed_at: new Date(),
+      })
+    );
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById("todo") as HTMLElement).render(<App />);
+function Index() {
+  return (
+    <>
+      <CssBaseline />
+      <App />
+    </>
+  );
+}
+
+createRoot(document.getElementById("todo") as HTMLElement).render(<Index />);
