ca905f49791afd171dfb8e59e0549db07d8be9fd
SaburovBabur <saburovbabur@gmail.com>
2022-04-14 18:31:52 +0500

    ESLint support

--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grid } from "@mui/material";
+import { Container } from "@mui/material";
 import TodoList from "../#/todos/TodoList";
 import Navbar from "../#/todos/Navbar";
 import TodoAddForm from "../#/todos/TodoAddForm";
